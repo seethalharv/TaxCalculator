f2f7e97f7bbf8af21bacaaf3f867bfee66de7502
Seethal Harvey <seethalmd@gmail.com>
2025-07-12 21:31:17 -0500

    Separated environment for prod and local.

--- TaxCalculator.UI/src/app/services/tax-calculator.service.ts
+++ TaxCalculator.UI/src/app/services/tax-calculator.service.ts
@@ -3,16 +3,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TaxInput } from '../models/tax-input.model';
 import { TaxResult } from '../models/tax-result.model';
+import { environment } from '../../environments/environment'; // Automatically swaps in prod
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaxCalculatorService {
-  private apiUrl = 'https://localhost:7072/api/taxcalculator/calculate';
+  private readonly baseUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {}
 
   calculateTax(input: TaxInput): Observable<TaxResult> {
-    return this.http.post<TaxResult>(this.apiUrl, input);
+   return this.http.post<TaxResult>(`${this.baseUrl}/api/taxcalculator/calculate`, input);
   }
 }
---
+++ TaxCalculator.UI/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://localhost:7072'  // Your local ASP.NET Core API
+};
