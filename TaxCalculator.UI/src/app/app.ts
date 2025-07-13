05c8e29f207165290a83a8888b82b02bf39057af
Seethal Harvey <seethalmd@gmail.com>
2025-07-13 16:13:42 -0500

    Ui enhancements.

--- TaxCalculator.UI/src/app/app.ts
+++ TaxCalculator.UI/src/app/app.ts
@@ -6,11 +6,22 @@ import { CommonModule } from '@angular/common';
 import { TaxResult } from './models/tax-result.model';
 import { TaxInput } from './models/tax-input.model';
 import { TaxCalculatorService } from './services/tax-calculator.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule,
+     FormsModule, 
+     MatProgressSpinnerModule, 
+     MatDividerModule,
+     MatCardModule, 
+     MatFormFieldModule,
+     MatInputModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
@@ -18,22 +29,31 @@ export class App {
   salary: number = 0;
   result!: TaxResult | null;
   error: string | null = null;
+  isLoading = false;
+  errorMessage = '';
 
   constructor(private taxService: TaxCalculatorService) {}
 
   calculate() {
     this.error = null;
     this.result = null;
+    this.errorMessage = '';
+    this.isLoading = true;
 
     if (!this.salary || this.salary <= 0) {
       this.error = 'Please enter a valid positive salary.';
+      this.isLoading = false;
       return;
     }
     const input: TaxInput = { salary: this.salary };
 
     this.taxService.calculateTax(input).subscribe({
-      next: (data : TaxResult) => this.result = data,
-      error: () => this.error = 'API call failed. Is the backend running? CORS might be blocking it.'
+      next: (data : TaxResult) => {this.result = data; 
+                                  this.isLoading = false;
+      },
+      error: () => {this.errorMessage = 'API call failed. Is the backend running? CORS might be blocking it.';
+                    this.isLoading = false;
+      }
     });
   }
 }
